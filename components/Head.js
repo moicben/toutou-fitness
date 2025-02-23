f1f965278bfca4e3159208fc65d30d2dc88c686d
moicben <benjamain.georges@gmail.com>
2025-02-23 14:17:48 +0400

    deploy test

--- components/Head.js
+++ components/Head.js
@@ -8,6 +8,15 @@ const myHead = ({ title, description, favicon }) => {
       <meta name="description" content={description} />
       <link rel="icon" href={favicon} />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+  
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16883090550"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-16883090550');
+      </script>
     </Head>
   );
 };
