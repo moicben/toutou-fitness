651ca89f88ba46e5746d53d7ab2d4611f75b306c
moicben <benjamain.georges@gmail.com>
2025-03-15 09:02:59 +0400

    tracking

--- components/Head.js
+++ components/Head.js
@@ -9,43 +9,33 @@ const MyHead = ({ title, description, favicon }) => {
       <link rel="icon" href={favicon} />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
   
-
-      {/* Google tag 1 (Initial)*/}
+      {/* Google tag 1 (Tout Fitness)*/}
       <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16883090550"></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16883090550');
+          `,
+        }}
+      />
 
-          gtag('config', 'AW-16883090550');
-        `}
-      </script>
-
-      {/* Google tag 2 */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16915371402"></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'AW-16915371402');
-        `}
-      </script>
-
-      <script>
-        {`
-          (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:5325018,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-      </script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:5325018,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
 
       <script src="https://js.mollie.com/v1/mollie.js"></script>
     </Head>
